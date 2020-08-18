84dff68be003d32c3e035d270c59687dbb05c618
HungNguyen81 <nguyenngochung.ncth@gmail.com>
2020-08-18 09:16:36 +0700

    ok

---
+++ action.js
@@ -0,0 +1,12 @@
+function clicked(){
+    alert("Cái form này chỉ để cho đẹp thôi hehe ^^");
+}
+
+function aboutme(){
+    var info = 'Name:  Nguyễn Ngọc Hưng \n'
+            +  'Email: nguyenngochung.ncth@gmail.com\n'
+            +  'MSSV:  20183759\n'
+            +  'Lớp: IT2-02\n'
+            +  'Khóa: 63-HUST';
+    alert(info);
+}
